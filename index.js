b3c8c5791fae98b4e420edb06ed10fc4078d1e40
michael2023-code <michael.mutunga@student.moringaschool.com>
2023-07-16 23:07:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(array) {
+    const winObject = array.find(obj => obj.result === "W");
+    return winObject ? winObject.year : undefined;
+  }
+
+  const reords= [
+    { year: 2015, result: "L" },
+    { year: 2014, result: "W" },
+    { year: 2013, result: "L" },
+  ];
+  
+  console.log(superbowlWin(records)); 
